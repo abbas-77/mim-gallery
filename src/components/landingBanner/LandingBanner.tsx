ab51ec6badaba2c09f935d56aeb88d2aaec49f3d
abbas-77 <abbas.azimii77@gmail.com>
2025-06-23 16:27:56 +0330

    fix jsx to tsx

--- src/components/landingBanner/LandingBanner.tsx
+++ src/components/landingBanner/LandingBanner.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LandingBanner = () => {
   return (
-    <div className='bg-[#0b150e] h-[70vh]'>LandingBanner</div>
+    <div className='bg-[#0b150e] h-[70vh]'>LandingBanner1</div>
   )
 }
 
