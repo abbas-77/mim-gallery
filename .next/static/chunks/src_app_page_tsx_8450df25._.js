04692052bdab5432e318146293d282aec4917eb1
abbas-77 <abbas.azimii77@gmail.com>
2025-06-25 13:32:29 +0330

    .

--- .next/static/chunks/src_app_page_tsx_8450df25._.js
+++ .next/static/chunks/src_app_page_tsx_8450df25._.js
@@ -2,7 +2,7 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/node_modules_5609a52a._.js",
-  "static/chunks/src_9d6c3145._.js"
+  "static/chunks/src_275b09bd._.js"
 ],
     source: "dynamic"
 });
